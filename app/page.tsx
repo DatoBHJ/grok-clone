ebcdf16f8f16b32155a31f19aed5fb56eb5003fd
datobhj <bdfutbol@naver.com>
2024-11-15 18:41:00 +0900

    deleting financial chart

--- app/page.tsx
+++ app/page.tsx
@@ -270,6 +270,11 @@ export default function Home() {
             </div>
           )}
 
+
+          {/* <p className="text-center dark:text-zinc-700 text-slate-300 text-md font-medium mt-2">
+            Groc can make mistakes. Verify its outputs.
+          </p> */}
+          
           <p className="text-center dark:text-zinc-700 text-slate-300 text-sm font-medium mt-2">
           Groc is an open source clone of xAI's Grok UI.<br />
           Important: Groc is 100% unaffiliated with xAI.
