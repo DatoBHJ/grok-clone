6d24f5370bdcbfd45ffd105c965577b6e023c54b
datobhj <bdfutbol@naver.com>
2024-11-20 05:54:40 +0900

    asdf

--- app/config.tsx
+++ app/config.tsx
@@ -1,5 +1,8 @@
 //app/config.tsx
 export const config = {
+    // BaseURL: 'https://api.groq.com/openai/v1',
+    // API_KEY: process.env.GROQ_API_KEY,
+    // Model: 'llama-3.1-70b-versatile',
     BaseURL: "https://api.x.ai/v1",
     API_KEY: process.env.XAI_API_KEY,
     Model: 'grok-beta',
