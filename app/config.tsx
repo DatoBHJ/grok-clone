ba2e07efb0b48fe0bca42bf77855f37889c2b644
datobhj <bdfutbol@naver.com>
2024-11-21 20:07:08 +0900

    rate limit

--- app/config.tsx
+++ app/config.tsx
@@ -1,17 +1,17 @@
 //app/config.tsx
 export const config = {
-    BaseURL: 'https://api.groq.com/openai/v1',
-    API_KEY: process.env.GROQ_API_KEY,
-    Model: 'llama-3.1-70b-versatile',
-    // BaseURL: "https://api.x.ai/v1",
-    // API_KEY: process.env.XAI_API_KEY,
-    // Model: 'grok-beta',
+    // BaseURL: 'https://api.groq.com/openai/v1',
+    // API_KEY: process.env.GROQ_API_KEY,
+    // Model: 'llama-3.1-70b-versatile',
+    BaseURL: "https://api.x.ai/v1",
+    API_KEY: process.env.XAI_API_KEY,
+    Model: 'grok-beta',
     fcBaseURL: 'https://api.groq.com/openai/v1',
     fcAPI_KEY: process.env.GROQ_API_KEY,
     fcModel: 'llama-3.1-70b-versatile',
     // fcModel: 'llama-3.1-8b-instant',
     useRateLimiting: true, 
-    numberOfPagesToScan: 1,
-    numberOfTweetToScan: 1,
+    numberOfPagesToScan: 10,
+    numberOfTweetToScan: 10,
 }
 
